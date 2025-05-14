255a7accbdb7a905cfab0e5b1d171b44fabb4fc5
StanislavSol <vandemar@bk.ru>
2025-05-14 13:52:37 +0300

    Initial commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email|unique:users',
+            'password' => ['required', Password::min(6)],
+            'gender' => 'nullable|in:male,female,other',
+        ]);
+
+        $user = User::create([
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+            'gender' => $validated['gender'] ?? null,
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+         return response()->json([
+             'message' => 'User registered successfully',
+             'user' => $user,
+             'access_token' => $token,  // Добавляем токен в ответ
+             'token_type' => 'Bearer',
+         ], 201);
+
+    }
+
+    public function profile(Request $request)
+    {
+        return response()->json([
+            'user' => $request->user(),
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/registration', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->get('/profile', [AuthController::class, 'profile']);
